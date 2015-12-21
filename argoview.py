68bd9fb7bd6875a9fb8964ea7d01c05b48ca8cac
urielz <uriel.zaja@gmail.com>
2015-12-20 20:46:18 -0700

    comment out section code

--- argoview.py
+++ argoview.py
@@ -273,6 +273,27 @@ for ii in range (1,31):
                     plt.close()
 
                     # plot section
+                    # sprof = np.loadtxt(sdir2+prof.split('_')[0][1:]+'_s.txt')
+                    #
+                    # sprof[1:,:][sprof[1:,:]>smax_check] = -1
+                    # sprof[1:,:][sprof[1:,:]<smin_check] = 'NaN'
+                    #
+                    # pres = sprof[0,:]
+                    # sec  = np.array([1,2,3])
+                    # #sal  = sprof[1:,:]
+                    #
+                    # sal_m = np.ma.masked_invalid(sprof[1:,:])
+                    #
+                    # #plt.pcolor(sec,-sprof[0,:],sprof[1:,:].T)
+                    # plt.pcolor(sec,-sprof[0,:],salm.T)
+                    # plt.clim([33.5,35])
+                    # plt.colorbar()
+                    # plt.ylabel('Pressure (dbar)')
+                    # plt.xlabel('Distance (Km)')
+                    #
+                    # plt.title("Salinity section - Float: "+str(prof.split('_')[0][1:]))
+                    # plt.savefig(sdir2+prof.split('_')[0][1:]+'.png',dpi=300)
+                    # plt.close()
 
             else:
 
