f6baad95ee522043e72536bb1a392183bf23957e
urielz <uriel.zaja@gmail.com>
2015-12-24 00:49:26 -0700

    turn figure to subplots

--- plot_section.py
+++ plot_section.py
@@ -1,4 +1,4 @@
-def plot_section(profname):
+def plot_section(profname,arg):
 
     import config
     import numpy as np
@@ -6,28 +6,25 @@ def plot_section(profname):
     from scipy.interpolate import griddata
 
     f_p  = open(config.sdir2+profname+'_p.txt','r')
-    f_s  = open(config.sdir2+profname+'_s.txt','r')
-    f_t  = open(config.sdir2+profname+'_t.txt','r')
     f_xy = open(config.tdir2+profname+'_traj.txt','r')
+
+    if arg == 0:
+        f_var  = open(config.sdir2+profname+'_t.txt','r')
+    if arg == 1:
+        f_var  = open(config.sdir2+profname+'_s.txt','r')
 
     p_arr, s_arr, t_arr, lat_arr, lon_arr, dist_arr = ([] for i in range(6))
 
     p_cnt = 0
     for p_line in f_p: # Loop over lines and extract variables of interest
         p_line = p_line.strip().split() # removes /n (strip) and converts str to list (split)
-        s_line = f_s.readline().strip().split()
-        t_line = f_t.readline().strip().split()
+        s_line = f_var.readline().strip().split()
         t_line = f_xy.readline().strip().split()
         lat_arr.append(float(t_line[1]))
         lon_arr.append(float(t_line[2]))
         dist_arr.append(float(t_line[3]))
 
         k = 0
-        # if p_cnt == 0:
-        #     dist = 0.
-        # else:
-        #     dist_prev = dist
-        #     dist = sw.dist((lat_arr[p_cnt],lat_arr[p_cnt-1]), (lon_arr[p_cnt],lon_arr[p_cnt-1]), units='km')[0] + dist_prev
 
         for i in p_line:
             if float(i)<9000:
@@ -39,7 +36,7 @@ def plot_section(profname):
             np_elem = np.size(p_arr)
         p_cnt = p_cnt + 1
     f_p.close()
-    f_s.close()
+    f_var.close()
 
     p_arr = -np.asarray(p_arr)
     s_arr = np.asarray(s_arr)
@@ -55,10 +52,14 @@ def plot_section(profname):
     CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.jet)
 
     plt.colorbar() # draw colorbar
-    plt.scatter(t_arr,p_arr,marker='o',c='b',s=3) # draw locations of observations
+    plt.scatter(t_arr,p_arr,marker='o',c='b',s=1) # draw locations of observations
     plt.grid()
-    plt.ylabel('Pressure (dbar)')
-    plt.xlabel('Distance (Km)')
-    plt.title('Salinity section - Float: '+profname)
+    plt.ylabel('Pressure (dbar)',fontsize=5)
+    plt.xlabel('Distance (Km)',fontsize=5)
+
+    if arg == 0:
+        plt.title('Temperature section - Float: '+profname,fontsize=5)
+    if arg == 1:
+        plt.title('Salinity section - Float: '+profname,fontsize=5)
 
     return;
